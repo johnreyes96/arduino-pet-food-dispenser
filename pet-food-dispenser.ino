8f534f43abb327288e56febd9401d25248a46a71
johnfreyes <jhonfer96@hotmail.com>
2021-11-12 23:48:17 -0500

    fixed technical debt and code smeel

--- pet-food-dispenser.ino
+++ pet-food-dispenser.ino
@@ -6,50 +6,108 @@
 #define ABRIR 90
 
 LiquidCrystal lcd(A0, A1, A2, A3, A4, A5);
-const byte columns = 4;
-const byte rows = 4;
-Servo servoMotor;
-char inKeys;
-byte rowPins[rows] = { 7, 6, 5, 4 };
-byte columnPins[columns] = { 3, 2, 1, 0 };
-char keys[rows][columns] = 
+const byte COLUMNS = 4;
+const byte ROWS = 4;
+const byte LED = 9;
+const byte PIEZO = 10;
+const char KEYS[ROWS][COLUMNS] = 
 {
   { '1', '2', '3', 'A' },
   { '4', '5', '6', 'B' },
   { '7', '8', '9', 'C' },
   { '*', '0', '#', 'D' },
 };
-Keypad keyboard = Keypad(makeKeymap(keys), rowPins, columnPins, rows, columns);
+byte _rowPins[ROWS] = { 7, 6, 5, 4 };
+byte _columnPins[COLUMNS] = { 3, 2, 1, 0 };
+Keypad _keyboard = Keypad(makeKeymap(KEYS), _rowPins, _columnPins, ROWS, COLUMNS);
+Servo _servoMotor;
+String messages[2];
 
 void setup()
 {
-  servoMotor.attach(8);
-  servoMotor.write(CERRAR);
-  pinMode(9, OUTPUT);
-  digitalWrite(9, LOW);
-  pinMode(10, OUTPUT);
-  digitalWrite(10, HIGH);
-
-  lcd.begin(16, 2);
-  lcd.setCursor(0, 0);
-  lcd.print("Puerta cerrada");
-  lcd.setCursor(0, 1);
-  lcd.print("holiii");
+  _initServomotor();
+  _initLed();
+  _initPiezo();
+  _initLCDDisplay();
 }
 
 void loop()
 {
-  inKeys = keyboard.getKey();
-  switch (inKeys) {
+  _executeFunctionByKeypad();
+}
+
+void _executeFunctionByKeypad()
+{
+  switch (_getKey()) {
     case 'A':
-      servoMotor.write(ABRIR);
-      digitalWrite(9, HIGH);
-      digitalWrite(10, LOW);
+      _foodDispenser();
       break;
     case 'B':
-      servoMotor.write(CERRAR);
-      digitalWrite(9, LOW);
-      digitalWrite(10, HIGH);
+      _setInitialStatus();
       break;
   }
+}
+
+void _foodDispenser()
+{
+  _servoMotor.write(ABRIR);
+  digitalWrite(LED, HIGH);
+  digitalWrite(PIEZO, LOW);
+  messages[0] = "   Dispensando  ";
+  messages[1] = "     Comida     ";
+  _showMessageInLCDDisplay(messages);
+}
+
+void _setInitialStatus()
+{
+  _servoMotor.write(CERRAR);
+  digitalWrite(LED, LOW);
+  digitalWrite(PIEZO, HIGH);
+  delay(600);
+  _showMainMessageInLCDDisplay();
+}
+
+char _getKey()
+{
+  return _keyboard.getKey();
+}
+
+void _initServomotor()
+{
+  _servoMotor.attach(8);
+  _servoMotor.write(CERRAR);
+}
+
+void _initLed()
+{
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+}
+
+void _initPiezo()
+{
+  pinMode(PIEZO, OUTPUT);
+  digitalWrite(PIEZO, HIGH);
+}
+
+void _initLCDDisplay()
+{
+  lcd.begin(16, 2);
+  _showMainMessageInLCDDisplay();
+}
+
+void _showMainMessageInLCDDisplay()
+{
+  lcd.setCursor(0, 0);
+  lcd.print("A: Para abrir   ");
+  lcd.setCursor(0, 1);
+  lcd.print("B: Para cerrar  ");
+}
+
+void _showMessageInLCDDisplay(String message[])
+{
+  lcd.setCursor(0, 0);
+  lcd.print(message[0]);
+  lcd.setCursor(0, 1);
+  lcd.print(message[1]);
 }
